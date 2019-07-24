26f1ae5d7ff038cfeb847167cc06e693ee104fb3
FANTOME <indv.panh@gmail.com>
2019-07-24 22:20:30 +0800

    Add Audios & Physical Effects
    加了吃星星、撞岩石、转圈和撞到雪地的音效。
    加了撞到岩石的物理效果。

--- Assets/Scripts/GamePlay/PlayerCollider.cs
+++ Assets/Scripts/GamePlay/PlayerCollider.cs
@@ -5,21 +5,31 @@
  */
 public class PlayerCollider : MonoBehaviour
 {
-  /*
-   * A trigger callback to detect when the player's collider has
-   * entered the obstacle's. Then simply obtain the PlayerController
-   * reference can apply damage. Then remove the obstacle for feedback.
-   */
-  private void OnTriggerEnter2D(Collider2D other)
-  {
-    // Obtain a reference to the Player's PlayerController
-    PlayerController playerController =
-      other.attachedRigidbody.gameObject.GetComponent<PlayerController>();
+    public AudioClip collisionAudio;
+    public float MinCollisionForce = 1f;
+    public float MaxCollisionForce = 5f;
 
-    // Register damage with player
-    playerController.Damage();
+    /*
+     * A trigger callback to detect when the player's collider has
+     * entered the obstacle's. Then simply obtain the PlayerController
+     * reference can apply damage. Then remove the obstacle for feedback.
+     */
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Obtain a reference to the Player's PlayerController
+        PlayerController playerController =
+          other.attachedRigidbody.gameObject.GetComponent<PlayerController>();
 
-    // Make this object disappear
-    GameObject.Destroy(gameObject);
-  }
+        // Physical Effect
+        Vector2 dir = (other.transform.position - transform.position).normalized;
+        float force = Random.Range(MinCollisionForce, MaxCollisionForce);
+        other.attachedRigidbody.AddForceAtPosition(force * dir, transform.position, ForceMode2D.Impulse);
+
+        // Register damage with player
+        playerController.Damage();
+
+        AudioPlayer.PlayAudio(collisionAudio, 0.6f);
+        // Make this object disappear
+        GameObject.Destroy(gameObject);
+    }
 }
